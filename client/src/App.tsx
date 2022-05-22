2467439f28803d86155954ae63d01948e8247910
GigaHierz <lenahierzi@hotmail.de>
2022-05-22 02:58:00 -0400

    Add info to heading and favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,56 +1,41 @@
-import React from 'react';
-import { useEffect } from "react"
+import React from 'react'
+import { useEffect } from 'react'
 
-import logo from './logo.svg';
+import logo from './logo.svg'
 //import './assets/css/fonts.css';
 
-import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 import { useWeb3React } from '@web3-react/core'
 
+import Navigation from './components/Navigation'
+import ProfilePage from './Pages/ProfilePage'
+import ProfileCard from './components/ProfileCard'
 
-
-
-
-import Navigation from './components/Navigation';
-import ProfilePage from './Pages/ProfilePage';
-import ProfileCard from './components/ProfileCard';
-
-
-
-import './App.css';
+import './App.css'
 
 const CoinbaseWallet = new WalletLinkConnector({
   url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-  appName: "Web3-react Demo",
-  supportedChainIds: [1, 3, 4, 5, 42],
- });
+  appName: 'ShaDonnas List',
+  supportedChainIds: [1, 3, 4, 5, 42]
+})
 
-
- 
-
-function App() {
-
-  const { activate, deactivate } = useWeb3React();
-  const { active, chainId, account } = useWeb3React();
-
-  
+function App () {
+  const { activate, deactivate } = useWeb3React()
+  const { active, chainId, account } = useWeb3React()
 
   //const { active, account, library, connector, activate, deactivate } = useWeb3React()
 
-
   useEffect(() => {
-    localStorage.clear();
-
+    localStorage.clear()
 
     //function to connect wallet when the page loads
     const connectWalletOnPageLoad = async () => {
-
       //if wallet is already connected -- then load
       if (localStorage?.getItem('isWalletConnected') === 'true') {
         try {
           await activate(CoinbaseWallet)
-          localStorage.setItem('isWalletConnected', "true")
+          localStorage.setItem('isWalletConnected', 'true')
         } catch (ex) {
           console.log(ex)
         }
@@ -59,20 +44,19 @@ function App() {
     //connectWalletOnPageLoad()
   }, [])
 
-
-  async function connect() {
+  async function connect () {
     try {
-      await activate(CoinbaseWallet) 
-      
+      await activate(CoinbaseWallet)
+
       //console.log(CoinbaseWallet)
-      console.log("connected")
+      console.log('connected')
       //localStorage.setItem('isWalletConnected', "true")
     } catch (ex) {
       console.log(ex)
     }
   }
 
-  async function disconnect() {
+  async function disconnect () {
     try {
       deactivate()
       //localStorage.setItem('isWalletConnected', false)
@@ -81,22 +65,22 @@ function App() {
     }
   }
 
-
   return (
-    <div className="App">
-      <Navigation connectwallet={connect} disconnectwallet={disconnect} active={active}></Navigation>
+    <div className='App'>
+      <Navigation
+        connectwallet={connect}
+        disconnectwallet={disconnect}
+        active={active}
+      ></Navigation>
       {/* <button onClick={connect}>Coinbase Wallet</button>
       <button onClick={deactivate}>Disconnect</button>
       <div>{`Connection Status: ${active}`}</div>
       <div>{`Account: ${account}`}</div>
       <div>{`Network ID: ${chainId}`}</div> */}
-     
-
-
 
       <ProfilePage></ProfilePage>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import logo from './logo.svg'
 import '../styles/navigation.css'
 
 interface NavigationProps {
-  connectwallet: (params: any) => any;
-  disconnectwallet: (params: any) => any;
-  active: any;
+  connectwallet: (params: any) => any
+  disconnectwallet: (params: any) => any
+  active: any
 }
 
-function Navigation({connectwallet, disconnectwallet, active}:NavigationProps) {
+function Navigation ({
+  connectwallet,
+  disconnectwallet,
+  active
+}: NavigationProps) {
   return (
-    <div >
-      <ul className="nav-list">
-      <li className="nav-title-item"><a href="#home">ShaDonna's List</a></li>
+    <div>
+      <ul className='nav-list'>
+        <li className='nav-title-item'>
+          <a href='#home'>ShaDonna's List - Rating The Block</a>
+        </li>
         {/* <li className="nav-list-item"><a href="#home">Home</a></li>
         <li className="nav-list-item"><a href="#news">News</a></li>
         <li className="nav-list-item"><a href="#contact">Contact</a></li> */}
-        {!active? 
-        <li onClick={connectwallet} className="float-right nav-list-item connect-wallet-button"><a href="#about">Connect Wallet</a></li>:
-        <li onClick={disconnectwallet} className="float-right nav-list-item disconnect-wallet-button"><a href="#about">Disconnect Waller</a></li>
-        }
+        {!active ? (
+          <li
+            onClick={connectwallet}
+            className='float-right nav-list-item connect-wallet-button'
+          >
+            <a href='#about'>Connect Wallet</a>
+          </li>
+        ) : (
+          <li
+            onClick={disconnectwallet}
+            className='float-right nav-list-item disconnect-wallet-button'
+          >
+            <a href='#about'>Disconnect Waller</a>
+          </li>
+        )}
 
         {/* <button onClick={connect}>Coinbase Wallet</button> */}
       </ul>
     </div>
-  );
+  )
 }
 
-export default Navigation;
+export default Navigation
