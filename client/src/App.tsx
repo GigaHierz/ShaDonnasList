29316c721e91764f1233f0b90cac3427c3c3bca0
kitan <oademid2@uwo.ca>
2022-05-22 04:55:49 -0400

    add metamask wallet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useEffect } from "react"
+import { ethers } from 'ethers';
+import { useState } from 'react';
 
 import logo from './logo.svg';
 //import './assets/css/fonts.css';
 
 import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+//import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
 
 import { useWeb3React } from '@web3-react/core'
 
+// import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+// import WalletConnect from "@walletconnect/web3-provider";
+
+// import Web3Modal from "web3modal";
 
 
 
@@ -15,6 +24,8 @@ import { useWeb3React } from '@web3-react/core'
 import Navigation from './components/Navigation';
 import ProfilePage from './Pages/ProfilePage';
 import ProfileCard from './components/ProfileCard';
+
+
 
 
 
@@ -26,6 +37,36 @@ const CoinbaseWallet = new WalletLinkConnector({
   supportedChainIds: [1, 3, 4, 5, 42],
  });
 
+//  const WalletConnect = new WalletConnectConnector({
+//   rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+//   bridge: "https://bridge.walletconnect.org",
+//   qrcode: true,
+//  });
+ 
+const Injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42]
+ });
+
+//  export const providerOptions = {
+//  coinbasewallet: {
+//    package: CoinbaseWalletSDK, 
+//    options: {
+//      appName: "Web 3 Modal Demo",
+//      infuraId: process.env.INFURA_KEY 
+//    }
+//  },
+//  walletconnect: {
+//    package: WalletConnect, 
+//    options: {
+//      infuraId: process.env.INFURA_KEY 
+//    }
+//  }
+// };
+
+// const web3Modal = new Web3Modal({
+//   providerOptions // required
+// });
+
 
  
 
@@ -33,6 +74,9 @@ function App() {
 
   const { activate, deactivate } = useWeb3React();
   const { active, chainId, account } = useWeb3React();
+
+  const [provider, setProvider] = useState();
+  const [library, setLibrary] = useState();
 
   
 
@@ -60,9 +104,20 @@ function App() {
   }, [])
 
 
-  async function connect() {
+  async function connect(wallet:any) {
     try {
-      await activate(CoinbaseWallet) 
+
+      console.log(wallet)
+
+      if(wallet=="coinbase"){
+        await activate(CoinbaseWallet) 
+              console.log("cb")
+
+      }else{
+        await activate(Injected) 
+        console.log("mm")
+
+      }
       
       //console.log(CoinbaseWallet)
       console.log("connected")
@@ -82,14 +137,19 @@ function App() {
   }
 
 
+
+
+
   return (
     <div className="App">
       <Navigation connectwallet={connect} disconnectwallet={disconnect} active={active}></Navigation>
-      {/* <button onClick={connect}>Coinbase Wallet</button>
+      <button onClick={connect}>Coinbase Wallet</button>
       <button onClick={deactivate}>Disconnect</button>
       <div>{`Connection Status: ${active}`}</div>
       <div>{`Account: ${account}`}</div>
-      <div>{`Network ID: ${chainId}`}</div> */}
+      <div>{`Network ID: ${chainId}`}</div>
+
+      
      
 
 
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -17,7 +17,12 @@ function Navigation({connectwallet, disconnectwallet, active}:NavigationProps) {
         <li className="nav-list-item"><a href="#news">News</a></li>
         <li className="nav-list-item"><a href="#contact">Contact</a></li> */}
         {!active? 
-        <li onClick={connectwallet} className="float-right nav-list-item connect-wallet-button"><a href="#about">Connect Wallet</a></li>:
+        <div>
+          
+        <li onClick={()=>connectwallet("coinbase")} className="float-right nav-list-item connect-wallet-button"><a href="#about">Connect Coinbase Wallet</a></li>
+        <li onClick={()=>connectwallet("walletconnect")} className="float-right nav-list-item connect-wallet-button"><a href="#about">Connect Metamask Wallet</a></li>
+
+        </div>:
         <li onClick={disconnectwallet} className="float-right nav-list-item disconnect-wallet-button"><a href="#about">Disconnect Waller</a></li>
         }
 
